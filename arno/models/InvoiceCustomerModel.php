8707805e0bdcf86a73963a15dd7898b8edfcb247
jeckryouhk <mr.jeck.ryo@gmail.com>
2018-02-20 16:38:18 +0700

    Update

---
+++ arno/models/InvoiceCustomerModel.php
@@ -0,0 +1,224 @@
+<?php
+
+require_once("BaseModel.php");
+class InvoiceSupplierModel extends BaseModel{
+
+    function __construct(){
+        $this->db = mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
+    }
+
+    function getInvoiceSupplierBy($date_start  = '', $date_end  = '', $status ="Waiting"){
+        $sql = " SELECT invoice_supplier_id, 
+        invoice_supplier_code, 
+        invoice_supplier_date, 
+        invoice_supplier_total_price,
+        invoice_supplier_vat_price,
+        invoice_supplier_net_price,
+        IFNULL(CONCAT(tb1.user_name,' ',tb1.user_lastname),'-') as employee_name, 
+        invoice_supplier_term, 
+        invoice_supplier_due, 
+        IFNULL(CONCAT(tb2.supplier_name_en,' (',tb2.supplier_name_th,')'),'-') as supplier_name  
+        FROM tb_invoice_supplier 
+        LEFT JOIN tb_user as tb1 ON tb_invoice_supplier.employee_id = tb1.user_id 
+        LEFT JOIN tb_supplier as tb2 ON tb_invoice_supplier.supplier_id = tb2.supplier_id 
+        ORDER BY STR_TO_DATE(invoice_supplier_date,'%Y-%m-%d %H:%i:%s') DESC 
+         ";
+        if ($result = mysqli_query($this->db,$sql, MYSQLI_USE_RESULT)) {
+            $data = [];
+            while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+                $data[] = $row;
+            }
+            $result->close();
+            return $data;
+        }
+
+    }
+
+    function getInvoiceSupplierByID($id){
+        $sql = " SELECT * 
+        FROM tb_invoice_supplier 
+        LEFT JOIN tb_supplier ON tb_invoice_supplier.supplier_id = tb_supplier.supplier_id 
+        LEFT JOIN tb_user ON tb_invoice_supplier.employee_id = tb_user.user_id 
+        WHERE invoice_supplier_id = '$id' 
+        ";
+
+        if ($result = mysqli_query($this->db,$sql, MYSQLI_USE_RESULT)) {
+            $data;
+            while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+                $data = $row;
+            }
+            $result->close();
+            return $data;
+        }
+
+    }
+
+    function getInvoiceSupplierViewByID($id){
+        $sql = " SELECT *   
+        FROM tb_invoice_supplier 
+        LEFT JOIN tb_user ON tb_invoice_supplier.employee_id = tb_user.user_id 
+        LEFT JOIN tb_user_position ON tb_user.user_position_id = tb_user.user_position_id 
+        LEFT JOIN tb_supplier ON tb_invoice_supplier.supplier_id = tb_supplier.supplier_id 
+        WHERE invoice_supplier_id = '$id' 
+        ";
+
+        if ($result = mysqli_query($this->db,$sql, MYSQLI_USE_RESULT)) {
+            $data;
+            while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+                $data = $row;
+            }
+            $result->close();
+            return $data;
+        }
+
+    }
+
+
+   
+    function updateInvoiceSupplierByID($id,$data = []){
+        $sql = " UPDATE tb_invoice_supplier SET 
+        supplier_id = '".$data['supplier_id']."', 
+        employee_id = '".$data['employee_id']."', 
+        invoice_supplier_code = '".$data['invoice_supplier_code']."', 
+        invoice_supplier_total_price = '".$data['invoice_supplier_total_price']."', 
+        invoice_supplier_vat = '".$data['invoice_supplier_vat']."', 
+        invoice_supplier_vat_price = '".$data['invoice_supplier_vat_price']."', 
+        invoice_supplier_net_price = '".$data['invoice_supplier_net_price']."', 
+        invoice_supplier_date = '".$data['invoice_supplier_date']."', 
+        invoice_supplier_date_recieve = '".$data['invoice_supplier_date_recieve']."', 
+        invoice_supplier_name = '".$data['invoice_supplier_name']."', 
+        invoice_supplier_address = '".$data['invoice_supplier_address']."', 
+        invoice_supplier_tax = '".$data['invoice_supplier_tax']."', 
+        invoice_supplier_term = '".$data['invoice_supplier_term']."', 
+        invoice_supplier_due = '".$data['invoice_supplier_due']."', 
+        updateby = '".$data['updateby']."', 
+        lastupdate = '".$data['lastupdate']."' 
+        WHERE invoice_supplier_id = $id 
+        ";
+
+        if (mysqli_query($this->db,$sql, MYSQLI_USE_RESULT)) {
+           return true;
+        }else {
+            return false;
+        }
+
+
+    }
+
+
+    function getSupplierOrder(){
+
+        $sql = "SELECT tb_supplier.supplier_id, supplier_name_en , supplier_name_th 
+                FROM tb_supplier 
+                WHERE supplier_id IN ( 
+                    SELECT DISTINCT supplier_id 
+                    FROM tb_purchase_order 
+                    LEFT JOIN tb_purchase_order_list ON tb_purchase_order.purchase_order_id = tb_purchase_order_list.purchase_order_id
+                    WHERE invoice_supplier_list_id = 0 
+                ) 
+        ";
+        $data = [];
+        if ($result = mysqli_query($this->db,$sql, MYSQLI_USE_RESULT)) {
+            
+            while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+                $data[] = $row;
+            }
+            $result->close();
+            
+        }
+        return $data;
+    }
+
+    function generateInvoiceSupplierListBySupplierId($supplier_id){
+
+       
+
+        $sql_customer = "SELECT tb_purchase_order_list.product_id, 
+        purchase_order_list_id,
+        CONCAT(product_code_first,product_code) as product_code, 
+        product_name, 
+        (invoice_supplier_list_qty - invoice_supplier_list_hold) as invoice_supplier_list_qty, 
+        product_buyprice as invoice_supplier_list_price ,
+        CONCAT('Order for customer purchase order ',invoice_supplier_code) as invoice_supplier_list_remark 
+        FROM tb_invoice_supplier 
+        LEFT JOIN tb_invoice_supplier_list ON tb_invoice_supplier.invoice_supplier_id = tb_invoice_supplier_list.invoice_supplier_id 
+        LEFT JOIN tb_product ON tb_invoice_supplier_list.product_id = tb_product.product_id 
+        LEFT JOIN tb_product_supplier ON tb_invoice_supplier_list.product_id = tb_product_supplier.product_id 
+        WHERE supplier_id = '$supplier_id' 
+        AND invoice_supplier_list_id = 0 
+        AND invoice_supplier_list_qty - invoice_supplier_list_hold > 0
+        AND product_supplier_status = 'Active' ";
+
+        //echo $sql_customer."<br><br>";
+        if ($result = mysqli_query($this->db,$sql_customer, MYSQLI_USE_RESULT)) {
+            
+            while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+                $data[] = $row;
+            }
+            $result->close();
+            
+        }
+
+        return $data;
+    }
+
+    function insertInvoiceSupplier($data = []){
+        $sql = " INSERT INTO tb_invoice_supplier (
+            supplier_id,
+            employee_id,
+            invoice_supplier_accept_status,
+            invoice_supplier_accept_by,
+            invoice_supplier_accept_date,
+            invoice_supplier_status,
+            invoice_supplier_type,
+            invoice_supplier_code,
+            invoice_supplier_credit_term,
+            invoice_supplier_delivery_term,
+            invoice_supplier_delivery_by,
+            invoice_supplier_date,
+            addby,
+            adddate,
+            updateby,
+            lastupdate) 
+        VALUES ('".
+        $data['supplier_id']."','".
+        $data['employee_id']."','".
+        $data['invoice_supplier_accept_status']."','".
+        $data['invoice_supplier_accept_by']."','".
+        $data['invoice_supplier_accept_date']."','".
+        $data['invoice_supplier_status']."','".
+        $data['invoice_supplier_type']."','".
+        $data['invoice_supplier_code']."','".
+        $data['invoice_supplier_credit_term']."','".
+        $data['invoice_supplier_delivery_term']."','".
+        $data['invoice_supplier_delivery_by']."','".
+        $data['invoice_supplier_date']."','".
+        $data['addby']."',".
+        "NOW(),'".
+        $data['addby'].
+        "',NOW()); 
+        ";
+
+
+        if (mysqli_query($this->db,$sql, MYSQLI_USE_RESULT)) {
+            return mysqli_insert_id($this->db);
+        }else {
+            return 0;
+        }
+
+    }
+
+
+    function deleteInvoiceSupplierByID($id){
+
+        $sql = " UPDATE tb_invoice_supplier_list SET invoice_supplier_list_id = '0' WHERE invoice_supplier_list_id (SELECT invoice_supplier_list_id FROM tb_invoice_supplier_list WHERE invoice_supplier_id = '$id') ";
+        mysqli_query($this->db,$sql, MYSQLI_USE_RESULT);
+
+        $sql = " DELETE FROM tb_invoice_supplier WHERE invoice_supplier_id = '$id' ";
+        mysqli_query($this->db,$sql, MYSQLI_USE_RESULT);
+        $sql = " DELETE FROM tb_invoice_supplier_list WHERE invoice_supplier_id = '$id' ";
+        mysqli_query($this->db,$sql, MYSQLI_USE_RESULT);
+
+    }
+}
+?>

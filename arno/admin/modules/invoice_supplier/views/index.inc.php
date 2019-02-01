043f6a4c6b7350c44f06896673766d068a7a4242
saksikon <saksikon@hotmail.com>
2019-02-01 11:32:06 +0700

    session update

--- arno/admin/modules/invoice_supplier/views/index.inc.php
+++ arno/admin/modules/invoice_supplier/views/index.inc.php
@@ -90,14 +90,14 @@ if(!isset($_GET['action']) && ( $license_purchase_page == "Medium" || $license_p
         $_SESSION['date_end'] = $date_end;
     }
 
-    if(!isset($_GET['keyword'])){
-        $keyword = $_SESSION['keyword'];
-    }else{
+    // if(!isset($_GET['keyword'])){
+    //     $keyword = $_SESSION['keyword'];
+    // }else{
         
-        $keyword = $_GET['keyword']; 
-        $_SESSION['keyword'] = $keyword;
-    }
-
+    //     $keyword = $_GET['keyword']; 
+    //     $_SESSION['keyword'] = $keyword;
+    // }
+    $keyword = $_GET['keyword'];
     
     if($date_start == ""){
         $date_start = date('01-m-Y'); 
